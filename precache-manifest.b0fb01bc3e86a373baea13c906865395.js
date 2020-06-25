d848e900256b1b40686476789755ee3e20eebac6
Kimleang Nao <kimleangn@gmail.com>
2020-06-25 12:51:37 -0700

    Updates

---
+++ precache-manifest.b0fb01bc3e86a373baea13c906865395.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02de641cb78287f4e441ca46f232f2b9",
+    "url": "/lolchamp/index.html"
+  },
+  {
+    "revision": "cbd3fa0bebe47ce2621a",
+    "url": "/lolchamp/static/css/main.8e157cd9.chunk.css"
+  },
+  {
+    "revision": "dace5173f1cb059c9377",
+    "url": "/lolchamp/static/js/2.a45a2a5d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/lolchamp/static/js/2.a45a2a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd3fa0bebe47ce2621a",
+    "url": "/lolchamp/static/js/main.a6d12965.chunk.js"
+  },
+  {
+    "revision": "34ffdf5d804364abc117",
+    "url": "/lolchamp/static/js/runtime-main.124ebb0c.js"
+  },
+  {
+    "revision": "ebe9b4fe27aa0c298debd7592210b97f",
+    "url": "/lolchamp/static/media/aatrox_big.ebe9b4fe.jpg"
+  },
+  {
+    "revision": "280e2ed438c5ae8998cb60723de506d1",
+    "url": "/lolchamp/static/media/aatroxs.280e2ed4.jpg"
+  },
+  {
+    "revision": "6d4de306d259ed74674fb6b508e3e086",
+    "url": "/lolchamp/static/media/ahri.6d4de306.jpg"
+  },
+  {
+    "revision": "e8cb79a5a7ac55fcef6c7fdeb776cfd4",
+    "url": "/lolchamp/static/media/ahri_big.e8cb79a5.jpg"
+  },
+  {
+    "revision": "84cb9ebeacc88bea5ee27dcc7294c56d",
+    "url": "/lolchamp/static/media/alistar.84cb9ebe.jpg"
+  },
+  {
+    "revision": "dff476d79317935886101a86865a59ee",
+    "url": "/lolchamp/static/media/alistar_big.dff476d7.jpg"
+  },
+  {
+    "revision": "27b4488317fca1e60c5e4382cf841feb",
+    "url": "/lolchamp/static/media/alistar_e.27b44883.png"
+  },
+  {
+    "revision": "dde8ec42b145ba35926102ddcad12bea",
+    "url": "/lolchamp/static/media/alistar_passive.dde8ec42.png"
+  },
+  {
+    "revision": "85c1ccda4f180d3adb3fb8b46c685fd2",
+    "url": "/lolchamp/static/media/alistar_r.85c1ccda.png"
+  },
+  {
+    "revision": "91a69842c8be0ff32ee41d0aed1ef0ea",
+    "url": "/lolchamp/static/media/amumu.91a69842.jpg"
+  },
+  {
+    "revision": "d84b8f53a78bf70e5034131eff72c149",
+    "url": "/lolchamp/static/media/amumu_big.d84b8f53.jpg"
+  },
+  {
+    "revision": "d3216679205728d634358f63c88b9a21",
+    "url": "/lolchamp/static/media/amumu_e.d3216679.png"
+  },
+  {
+    "revision": "eb5fe1dcb77361e8f3a40b4e5adb3fdf",
+    "url": "/lolchamp/static/media/amumu_passive.eb5fe1dc.png"
+  },
+  {
+    "revision": "3b25417fd9d1360aee6d9b4b215204b1",
+    "url": "/lolchamp/static/media/amumu_q.3b25417f.png"
+  },
+  {
+    "revision": "7b1a5fb51b7398f5677fc95506c9c66a",
+    "url": "/lolchamp/static/media/amumu_r.7b1a5fb5.png"
+  },
+  {
+    "revision": "f283ee31247131c30bbfc0e1d0b6bac0",
+    "url": "/lolchamp/static/media/amumu_w.f283ee31.png"
+  },
+  {
+    "revision": "13905d69acbf0c8f2b9afd8af2e3506f",
+    "url": "/lolchamp/static/media/aurelionsol.13905d69.jpg"
+  },
+  {
+    "revision": "8618510132d75d648c3cfe17ec20628a",
+    "url": "/lolchamp/static/media/aurelionsol_big.86185101.jpg"
+  },
+  {
+    "revision": "84857bf371734be68d90442c897c5985",
+    "url": "/lolchamp/static/media/aurelionsol_e.84857bf3.png"
+  },
+  {
+    "revision": "0d81894c45480e3cf1d78bea7e775d89",
+    "url": "/lolchamp/static/media/aurelionsol_passive.0d81894c.png"
+  },
+  {
+    "revision": "acc06633a00b9731c261d6619784fd05",
+    "url": "/lolchamp/static/media/aurelionsol_q.acc06633.png"
+  },
+  {
+    "revision": "94828385201589886a71e7fee7afd6d7",
+    "url": "/lolchamp/static/media/aurelionsol_r.94828385.png"
+  },
+  {
+    "revision": "9d79966c98a62287791ba423ee0fafef",
+    "url": "/lolchamp/static/media/aurelionsol_w.9d79966c.png"
+  },
+  {
+    "revision": "a582222a77dfe60973b237e0dc475b71",
+    "url": "/lolchamp/static/media/senna.a582222a.jpg"
+  },
+  {
+    "revision": "6440a55e67f0f0879446d9c6700f4248",
+    "url": "/lolchamp/static/media/senna_big.6440a55e.jpg"
+  },
+  {
+    "revision": "11f7090f059ce5fb45272c0d2a24cc5a",
+    "url": "/lolchamp/static/media/yasuo.11f7090f.jpg"
+  },
+  {
+    "revision": "522f14e917d448c38fd0b881cd0bf8c0",
+    "url": "/lolchamp/static/media/yasuo_big.522f14e9.jpg"
+  },
+  {
+    "revision": "696ce7c826b024180b5faaf1cd801594",
+    "url": "/lolchamp/static/media/yasuo_e.696ce7c8.png"
+  },
+  {
+    "revision": "e0f1aed17eae0c6cc0ef2793604cca51",
+    "url": "/lolchamp/static/media/yasuo_passive.e0f1aed1.png"
+  },
+  {
+    "revision": "28828f6da777adc4389bd553671e5f0b",
+    "url": "/lolchamp/static/media/yasuo_q.28828f6d.png"
+  },
+  {
+    "revision": "5e93843e3cf8417ce71a825cafcfc67f",
+    "url": "/lolchamp/static/media/yasuo_r.5e93843e.png"
+  },
+  {
+    "revision": "6d09f44f33fe4bebc0d91d0c8e38ae3f",
+    "url": "/lolchamp/static/media/yasuo_w.6d09f44f.png"
+  },
+  {
+    "revision": "eebfa1b6dbd00485d3f64f88489105ca",
+    "url": "/lolchamp/static/media/zed.eebfa1b6.jpg"
+  },
+  {
+    "revision": "c19eabf7873db8b3556bfdc44e4b34be",
+    "url": "/lolchamp/static/media/zed_big.c19eabf7.jpg"
+  },
+  {
+    "revision": "d0d91e002b61fda307ae2cc625bec982",
+    "url": "/lolchamp/static/media/zed_e.d0d91e00.png"
+  },
+  {
+    "revision": "fbe521a14569ac1136acc3a8926de7bc",
+    "url": "/lolchamp/static/media/zed_passive.fbe521a1.png"
+  },
+  {
+    "revision": "be90a8768ca7cbcf72109cf6255779ab",
+    "url": "/lolchamp/static/media/zed_q.be90a876.png"
+  },
+  {
+    "revision": "7f060bfc8784ed6ede13c1347353a91f",
+    "url": "/lolchamp/static/media/zed_r.7f060bfc.png"
+  },
+  {
+    "revision": "024ce6cc782a0bb74e3a379a4ad47e48",
+    "url": "/lolchamp/static/media/zed_w.024ce6cc.png"
+  }
+]);
